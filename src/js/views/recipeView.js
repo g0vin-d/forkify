8f2eaa2273347003465c3c9da96502f90ee80825
g0vin-d <krishhhello555@gmail.com>
2022-01-20 16:03:44 +0530

    fixed fractional lib bug

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from 'fraction.js';
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -131,7 +131,9 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
             </svg>
             <div class="recipe__quantity">${
-              ing.quantity ? new Fraction(+ing.quantity).toString() : ''
+              ing.quantity
+                ? new Fraction(ing.quantity).toFraction(true).toString()
+                : ''
             }</div>
             <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
